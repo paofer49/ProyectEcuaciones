da55c69f71bb7f808b96bbb57043aee1b65c9c70
PAOLA FERNANDA RUBI BACA <112739@alumnouninter.mx>
2024-11-28 23:39:56 -0600

    Add project files.

---
+++ ProyectEcuaciones/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectEcuaciones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------------------Euler------------------------------------------");
+            double x = 0;
+            double y = 3;
+            double h = 0.1;
+            Euler(x, y, h);
+
+            Console.WriteLine("---------------------Euler Mejorado--------------------------------");
+
+            double x2 = 1;
+            double y2 = 1;
+            EulerMejorado(x2, y2, h);
+
+            Console.ReadKey();
+        }
+
+        static double funcion(double x, double y) 
+        {
+            return x/y;
+        }
+
+        static void Euler(double x, double y, double h) 
+        {
+            for (int i = 0; i <15; i++)
+            {
+                Console.WriteLine($"x: {x}, y: {y}");
+                x += h;
+                y = y + h * (funcion(x, y));
+            }
+        }
+
+        static double funcion2(double x, double y) 
+        { 
+            return 2 * x * y;
+        }
+
+        static double yast(double x, double y, double h) 
+        { 
+            return y + (h * (funcion2(x, y)));
+        }
+
+        static void EulerMejorado(double x, double y, double h) 
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                Console.WriteLine($"x: {x}, y: {y}");
+                double sid = funcion2(x, y);
+                double sad =yast(x,y, h);
+                x += h;
+                y = y + h/2 * (sid + funcion2(x,sad));
+            }
+        }
+    }
+}
---
+++ ProyectEcuaciones/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ProyectEcuaciones")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ProyectEcuaciones")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("361272a0-3af1-4e78-9df9-2d5eb450a08f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
